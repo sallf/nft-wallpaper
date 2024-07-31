ccbb75cb34507ac0dc707f38b118937e3183e8f8
Saylor Allf <online@sallf.com>
2024-07-31 10:16:21 -0600

    style toggle btn

--- src/components/Toggle/ToggleBtn.tsx
+++ src/components/Toggle/ToggleBtn.tsx
@@ -1,8 +1,24 @@
-interface Props {}
+interface Props {
+  isActive: boolean
+  onClick: () => void
+  children: string
+}
 
-export const ToggleBtn = () => {
+export const ToggleBtn = (props: Props) => {
+  // --------------------- ===
+  //  PROPS
+  // ---------------------
+  const { isActive, onClick, children } = props
+
   // --------------------- ===
   //  RENDER
   // ---------------------
-  return <div></div>
+  return (
+    <button
+      type="button"
+      className={`rounded-full px-[14px] text-caption-md text-[#A6A3C8] transition-colors ${isActive ? 'bg-brand-white' : 'hover:text-brand-white'}`}
+    >
+      {children}
+    </button>
+  )
 }
