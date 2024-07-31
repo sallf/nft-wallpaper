8ee3b81f6214919dd9b82389d658f7e76e808b84
Saylor Allf <online@sallf.com>
2024-07-31 11:53:38 -0600

    add hero copy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,14 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-brand-midnight text-brand-white">
       <Header />
-      <div className="container mx-auto">
-        <Banner>Introducing NFT Wallpapers</Banner>
-        <h1 className="text-display-lg">CCO NFT Assets as Wallpapers</h1>
+      <div className="container mx-auto mt-12">
+        <div className="flex w-full flex-col gap-6 md:w-3/4">
+          <Banner>Introducing NFT Wallpapers</Banner>
+          <h1 className="text-display-lg">CCO NFT Assets as Wallpapers</h1>
+          <p className="text-brand-smokey">
+            Grab the NFT you like for your phone wallpaper
+          </p>
+        </div>
       </div>
     </main>
   )
